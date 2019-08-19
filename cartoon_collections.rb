2be8c5c3b41aa96a1d5d8a3a357776137c9bdbaa
Arlene George <aemg@live.com>
2019-08-19 02:38:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,7 @@ def summon_captain_planet(array)
   array.map! {|name| name.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
 end
 
 def find_the_cheese# code an argument here
