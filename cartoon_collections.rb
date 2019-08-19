6a6c5f2eb3cb2a72a4b7b1d5fe35bedf44a46b68
Arlene George <aemg@live.com>
2019-08-19 03:25:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
-    cheese_types.include? |item|
+    cheese_types.include?(item)
     
 end
